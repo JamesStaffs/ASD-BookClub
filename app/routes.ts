ee1c83bf19a48c6902b5661e56948924a02b77cd
James Stanley <james.stanley@staffs.ac.uk>
2025-09-11 12:15:25 +0100

    Update routes to use new config

--- app/routes.ts
+++ app/routes.ts
@@ -1,14 +1,14 @@
 import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import * as config from "./config";
 
 export default [
     index("routes/home.tsx"),
 
-    route("/auth/login", "routes/auth/login.tsx"),
-    // route("/auth/register", "routes/auth/register.tsx"),
-    route("/auth/logout", "routes/auth/logout.tsx"),
+    route(config.ROUTES.LOGIN, "routes/auth/login.tsx"),
+    route(config.ROUTES.LOGOUT, "routes/auth/logout.tsx"),
 
-    route("/lists", "routes/lists/index.tsx"),
-    route("/lists/new", "routes/lists/new.tsx"),
-    route("/lists/:id", "routes/lists/show.tsx"),
-    route("/lists/:id/edit", "routes/lists/edit.tsx"),
+    route(config.ROUTES.LISTS.INDEX, "routes/lists/index.tsx"),
+    route(config.ROUTES.LISTS.NEW, "routes/lists/new.tsx"),
+    route(config.ROUTES.LISTS.SHOW.PATTERN, "routes/lists/show.tsx"),
+    route(config.ROUTES.LISTS.EDIT.PATTERN, "routes/lists/edit.tsx"),
 ] satisfies RouteConfig;
